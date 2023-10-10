768e657fedbcdeec8d5af77921faf09dd65a1066
Paul Boniol <paul.boniol@free.fr>
2023-10-10 15:12:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,31 +20,25 @@ LOGGER = get_logger(__name__)
 
 def run():
     st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+        page_title="dSymb",
     )
 
     st.write("# Welcome to Streamlit! 👋")
 
-    st.sidebar.success("Select a demo above.")
+    tab_explore, tab_compare, tab_about = st.tabs(["Explore", "Compare", "About"])  
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    with tab_explore:
+        run_explore_frame()
+
+    with tab_compare:
+        run_compare_frame()
+
+    with tab_about:
+        run_about_frame()
+    
+    
+
+    
 
 
 if __name__ == "__main__":
