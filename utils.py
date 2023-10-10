1234f3be5a66135e3d86a406b0c17adcbcbc0234
Paul Boniol <paul.boniol@free.fr>
2023-10-10 15:15:33 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -18,15 +18,6 @@ import textwrap
 import streamlit as st
 
 
-def show_code(demo):
-    """Showing the code of the demo."""
-    show_code = st.sidebar.checkbox("Show code", True)
-    if show_code:
-        # Showing the code of the demo.
-        st.markdown("## Code")
-        sourcelines, _ = inspect.getsourcelines(demo)
-        st.code(textwrap.dedent("".join(sourcelines[1:])))
-
 
 def run_explore_frame():
     return st.markdown('# Explore')
