4edd591b849d72dad10b6ab914beddf8dc69e0ce
Paul Boniol <paul.boniol@free.fr>
2023-10-10 20:24:22 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -21,25 +21,25 @@ import streamlit as st
 
 
 def run_explore_frame():
-    	st.markdown('# Explore')
+	st.markdown('# Explore')
 	uploaded_ts = st.file_uploader("Upload your time series",accept_multiple_files=True)
 	if uploaded_ts is not None:
 		try:
             		all_ts = []
-            		for ts in uploaded_ts:
-                		all_ts.append(np.genfromtxt(ts, delimiter=','))
+			for ts in uploaded_ts:
+				all_ts.append(np.genfromtxt(ts, delimiter=','))
             
-            		time_series_selected = st.selectbox('Pick a time series', list(range(len(all_ts))))
-            		fig = plt.figure(10,50)
-            		for i in range(len(all_ts)):
-                		plt.subplot(len(all_ts),1,i+1)
-                		plt.plot(all_ts[i])
-            		st.pyplot(fig)
+			time_series_selected = st.selectbox('Pick a time series', list(range(len(all_ts))))
+			fig = plt.figure(10,50)
+			for i in range(len(all_ts)):
+				plt.subplot(len(all_ts),1,i+1)
+				plt.plot(all_ts[i])
+			st.pyplot(fig)
 
             
             
 		except Exception as e:
-            		st.markdown('file format not supported yet, please upload a time series in the format described in the about tab: {}'.format(e))
+			st.markdown('file format not supported yet, please upload a time series in the format described in the about tab: {}'.format(e))
     
 
 def run_compare_frame():
